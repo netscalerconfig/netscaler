075da3ed479ff0adafbbb6925cfeaf0473757e5e
Fabian Baena <fabian.baena@samanagroup.co>
2017-10-31 09:37:55 -0400

    modified path to libraries

--- tests/configtest.py
+++ tests/configtest.py
@@ -14,15 +14,19 @@ class TestAttribute(unittest.TestCase):
         ns.conf.add_server('server1', '10.0.1.98')
         ns.conf.add_service('testsvc1', '10.0.1.99', 'HTTP', '80')
         ns.conf.add_service('testsvc2', 'server1', 'HTTP', '80')
+        ns.conf.add_servicegroup('testsg1', 'HTTP')
         ns.conf.add_lb_vserver('test', 'HTTP', '10.0.0.1', '80')
         ns.conf.add_lb_vserver('test2', 'HTTP', '10.0.0.2', '80')
+        ns.conf.add_cs_vserver('testcs', 'HTTP', '10.0.0.3', '80')
         expectedout = \
             'add server 10.0.1.99 10.0.1.99 \n' + \
             'add server server1 10.0.1.98 \n' + \
             'add service testsvc2 server1 HTTP 80 \n' + \
             'add service testsvc1 10.0.1.99 HTTP 80 \n' + \
+            'add servicegroup testsg1 HTTP \n' + \
             'add lb vserver test HTTP 10.0.0.1 80 \n' + \
-            'add lb vserver test2 HTTP 10.0.0.2 80 \n'
+            'add lb vserver test2 HTTP 10.0.0.2 80 \n' + \
+            'add cs vserver testcs HTTP 10.0.0.3 80 \n'
 
         self.assertEqual(str(ns.conf), expectedout)
 
